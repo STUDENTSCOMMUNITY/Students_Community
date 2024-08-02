7cdecdc1920f36af997f1f78a296881d7e59300a
Jatin <chaurasiyajatin68@gmail.com>
2024-08-02 11:02:11 +0530

    major changes created all is working fine

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+app.use(cors());
+
+// Define Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ backend/src/utils/sendOtp.js
@@ -0,0 +1,6 @@
+const sendOtp = (otp, mobileNo) => {
+  console.log(`Sending OTP ${otp} to mobile number ${mobileNo}`);
+  // Integrate with an actual SMS API service like Twilio or any other
+};
+
+module.exports = sendOtp;
