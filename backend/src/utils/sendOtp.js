64441ea5534dfb73d65e4253771ba2ff2559afb5
Jatin <chaurasiyajatin68@gmail.com>
2024-08-09 17:22:23 +0530

    hello

--- backend/src/utils/sendOtp.js
+++
@@ -1,6 +0,0 @@
-const sendOtp = (otp, mobileNo) => {
-  console.log(`Sending OTP ${otp} to mobile number ${mobileNo}`);
-  // Integrate with an actual SMS API service like Twilio or any other
-};
-
-module.exports = sendOtp;
